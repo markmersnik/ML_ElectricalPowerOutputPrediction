3cc40d7d54e16c28362805004dd0ec485f548797
Mark Mersnik <Mark@Marks-iMac.local>
2019-12-04 22:18:17 -0800

    Added excel dataset and the basic python program

---
+++ data.py
@@ -0,0 +1,91 @@
+import xlrd
+import numpy as np
+import statistics
+from scipy.stats import linregress
+from matplotlib import pyplot
+
+loc = ("power_data.xlsx")
+
+wb = xlrd.open_workbook(loc)
+sheet = wb.sheet_by_index(0)
+sheet.cell_value(0, 0)
+
+#Training data
+first_col = sheet.col_values(0,1,7501)
+second_col = sheet.col_values(1,1,7501)
+third_col = sheet.col_values(2,1,7501)
+fourth_col = sheet.col_values(3,1,7501)
+fifth_col = sheet.col_values(4,1,7501)
+
+cols = [first_col,
+	second_col,
+	third_col,
+	fourth_col,
+	fifth_col]
+
+#Test data
+test1_col = sheet.col_values(0,7502,7652)
+test2_col = sheet.col_values(1,7502,7652)
+test3_col = sheet.col_values(2,7502,7652)
+test4_col = sheet.col_values(3,7502,7652) 
+test5_col = sheet.col_values(4,7502,7652) 
+
+test = [test1_col,
+	test2_col,
+	test3_col,
+	test4_col,
+	test5_col]
+
+#Labels of our different inputs and output
+l1 = sheet.col_values(0,0,1)
+l2 = sheet.col_values(1,0,1)
+l3 = sheet.col_values(2,0,1)
+l4 = sheet.col_values(3,0,1)
+l5 = sheet.col_values(4,0,1)
+
+labels = [l1, l2, l3, l4, l5]
+
+#Normalized data sets
+train_norm = []
+test_norm = []
+
+#Calculates all of the basic statistics for our inputs and output.
+x = 0
+for i in cols:
+
+	sum = 0
+	l = labels[x]
+
+	print(str(l[0]))
+	print("Mean: " + str(statistics.mean(i)))
+	print("Median: " + str(statistics.median(i)))
+	print("Mode: " + str(statistics.mode(i)))
+	print("Min: " + str(min(i)))
+	print("Max: " + str(max(i)))
+	print("Variance: " + str(statistics.variance(i)))
+	print("StDev: " + str(statistics.stdev(i)))
+	
+	#Calulcates the correlation coefficients
+	print("Corr. Coeff.: " + str(np.corrcoef(cols[4], i)) + "\n")
+
+	#Normalizes our data
+	norm_list = []
+	for num in i:
+		norm_list.append((num - min(i))/(max(i)-min(i)))
+	train_norm.append(norm_list)
+	x += 1
+
+#A scatter plot that shows linear regression
+x = train_norm[0]
+y = train_norm[4]
+
+print(linregress(x,y))
+
+coef = np.polyfit(x,y,1)
+poly1d_fn = np.poly1d(coef) 
+
+pyplot.plot(x,y, 'yo', x, poly1d_fn(x), '--k')
+pyplot.show()
+#Creates a scatter plot of our data.
+#pyplot.scatter(cols[0], cols[4])
+#pyplot.show()
